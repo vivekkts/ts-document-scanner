441c553380799df2403445f01c0905b6df2dd5bb
Navyashree K V <navyashree.kv@tallysolutions.com>
2025-02-24 15:27:21 +0530

    Include filename in arg

--- android/src/main/kotlin/ts/tally/document_scanner/fallback/DocumentScannerActivity.kt
+++ android/src/main/kotlin/ts/tally/document_scanner/fallback/DocumentScannerActivity.kt
@@ -248,7 +248,10 @@ class DocumentScannerActivity : AppCompatActivity() {
             filePath?.let { results.add(it) }
             setResult(
                 Activity.RESULT_OK,
-                Intent().putExtra("croppedImageResults", results)
+                Intent().apply{
+                    putExtra("croppedImageResults", results)
+                    putExtra("filename", filename)
+                }
             )
             finish()
             return
